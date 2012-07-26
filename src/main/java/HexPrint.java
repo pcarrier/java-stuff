88dc1dd01df17d20b07472590b10fb5a7da1910e
Pierre Carrier <pierre@spotify.com>
2012-07-26 11:14:24 +0000

    custom version, better bench

--- src/main/java/HexPrint.java
+++ src/main/java/HexPrint.java
@@ -12,8 +12,30 @@ public class HexPrint extends SimpleBenchmark {
     private Method method;
 
     private byte[] value;
+    static final byte[] hexBytes = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     public enum Method {
+        @SuppressWarnings("UnusedDeclaration")
+        CUSTOM {
+            @Override
+            final String convert(final byte[] in) {
+                final int length = in.length;
+                final byte[] out = new byte[2 * length];
+                for (int i = 0; i < length; i++) {
+                    final byte inByte = in[i];
+                    out[2*i] = hexBytes[(inByte & 0xf0) >> 4];
+                    out[2*i+1] = hexBytes[inByte & 0xf];
+                }
+                return new String(out);
+            }
+        },
+        @SuppressWarnings("UnusedDeclaration")
+        BIGINT {
+            @Override
+            final String convert(final byte[] in) {
+                return new BigInteger(1, in).toString(16);
+            }
+        },
         @SuppressWarnings("UnusedDeclaration")
         BUILDER {
             @Override
@@ -23,20 +45,12 @@ public class HexPrint extends SimpleBenchmark {
                     builder.append(String.format("%02x", b));
                 return builder.toString();
             }
-        },
-        @SuppressWarnings("UnusedDeclaration")
-        BIGINT {
-            @Override
-            final String convert(final byte[] in) {
-                return new BigInteger(1, in).toString(16);
-            }
-        };
+        },;
 
         abstract String convert(final byte[] in);
     }
 
-
-    public void timePrimitive(int reps) {
+    public void timeHexPrint(int reps) {
         for (int i = 0; i < reps; i++)
             method.convert(value);
     }
@@ -44,7 +58,7 @@ public class HexPrint extends SimpleBenchmark {
     @Override
     protected void setUp() {
         Random rnd = new Random();
-        value= new BigInteger(bits, rnd).toByteArray();
+        value = new BigInteger(bits, rnd).toByteArray();
     }
 
     public static void main(String[] args) throws Exception {
